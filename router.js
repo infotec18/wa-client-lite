e7124de9ccb43efa2d92e0e42aa2acd039f4c0d2
Renan G. Dutra <r.granatodutra@gmail.com>
2023-10-06 11:28:29 -0300

    feat: status route

--- router.js
+++ router.js
@@ -2,46 +2,52 @@ const { Router } = require("express");
 const WhatsappInstances = require("./instances.js");
 const path = require("path");
 const fs = require("fs");
+const multer = require('multer');
+const { logWithDate } = require("./utils.js");
 
 class AppRouter {
     router = Router();
 
     constructor() {
-        this.router.post("/messages/:from/:to", this.sendMessage);
-        this.router.get("files/:filename", this.getFile);
+        const upload = multer();
+
+        this.router.post("/messages/:from/:to", upload.single("file"), this.sendMessage);
+        this.router.get("/files/:filename", this.getFile);
+        this.router.get("", this.healthCheck);
+        this.router.get("/clients", this.getClientStatus);
     }
 
     async sendMessage(req, res) {
         try {
-            const [from, to] = req.params;
+            const { from, to } = req.params;
             const findInstance = WhatsappInstances.find(from);
 
             if (!findInstance) {
                 return res.status(404).json({ message: "Whatsapp number isn't found " });
             }
 
-            const [text, referenceId] = req.body;
-            const file = "";
+            const { text, referenceId } = req.body;
+            const file = req.file;
 
             if (file) {
                 const sentMessageWithFile = await findInstance.sendFile({
                     file: file.buffer,
                     fileName: file.originalname,
-                    mimeType: file.mimeType,
+                    mimeType: file.mimetype,
                     contact: to,
                     caption: text,
                     quotedMessageId: referenceId
                 });
 
-                return sentMessageWithFile;
+                res.status(201).json(sentMessageWithFile);
             } else {
                 const sentMessage = await findInstance.sendText(to, text, referenceId);
 
-                return sentMessage;
+                res.status(201).json(sentMessage);
             }
 
         } catch (err) {
-            console.error("[router.js] Send message failure =>", err);
+            logWithDate("[router.js] Send message failure =>", err);
         }
     }
 
@@ -50,13 +56,37 @@ class AppRouter {
             const fileName = req.params.filename;
             const filesPath = path.join(__dirname, "/files");
             const searchFilePath = path.join(filesPath, fileName);
+
+            if (!fs.existsSync(searchFilePath)) {
+                res.status(404).json({ message: "File not found" });
+            }
+
             const readStream = fs.createReadStream(searchFilePath);
+
             readStream.pipe(res);
             readStream.on('end', () => readStream.close());
 
-            console.log("Get file success =>", fileName);
+            logWithDate("Get file success =>", fileName);
         } catch (err) {
-            console.error("Get file failure =>", err);
+            logWithDate("Get file failure =>", err);
+
+            res.status(500).json({ message: "Something went wrong" });
+        }
+    }
+
+    async healthCheck(req, res) {
+        res.status(200).json({ online: true });
+    }
+
+    async getClientStatus(req, res) {
+        try {
+            const instances = WhatsappInstances.instances.map(async i => ({ number: i.whatsappNumber, auth: i.isAuthenticated, status: await i.client.getState() }));
+
+            logWithDate("Get clients statuses success!");
+            res.status(200).json({ instances });
+        } catch (err) {
+            logWithDate("Get clients statuses failure => ", err);
+            res.status(500).json({ message: "Something went wrong" });
         }
     }
 }

957fdb63260527c085dcb8cc0ba96bf5648fc070
Renan G Dutra <r.granatodutra@gmail.com>
2024-06-19 10:27:30 -0300

    feat: add search on contacts table

--- src/functions/loadContacts.ts
+++ src/functions/loadContacts.ts
@@ -24,10 +24,43 @@ export async function loadContacts(instance: WhatsappInstance) {
               (AREA3 = ${DDD} AND FONE3 = '${numberWithout9}')
             `;
 
-        const findCustomer = await instance.pool
-            .query(SEARCH_CUSTOMER_QUERY)
-            .then(res => (res[0] as RowDataPacket[])[0] || null)
-            .catch(err => console.error(err));
+        const SEARCH_CONTACT_QUERY = `
+            SELECT 
+                * 
+            FROM 
+                contatos
+            WHERE
+                (AREA_CEL = '${DDD}' AND CELULAR = '${numberWith9}') OR 
+                (AREA_CEL = '${DDD}' AND CELULAR = '${numberWithout9}') OR
+                (AREA_DIRETO = '${DDD}' AND FONE_DIRETO = '${numberWith9}') OR 
+                (AREA_DIRETO = '${DDD}' AND FONE_DIRETO = '${numberWithout9}') OR  
+                (AREA_RESI = '${DDD}' AND FONE_RESIDENCIAL = '${numberWith9}') OR 
+                (AREA_RESI = '${DDD}' AND FONE_RESIDENCIAL = '${numberWithout9}')
+            `;
+
+        const findCustomer = await new Promise<{ CODIGO: number } | null>(async (res) => {
+            const findOnCustomers = await instance.pool
+                .query<RowDataPacket[]>(SEARCH_CUSTOMER_QUERY)
+                .then(res => res[0][0] || null)
+                .catch(err => {
+                    console.error(err);
+                    return null;
+                });
+
+            if (findOnCustomers) res(findOnCustomers as { CODIGO: number });
+
+            const findOnContacts = await instance.pool
+                .query<RowDataPacket[]>(SEARCH_CONTACT_QUERY)
+                .then(res => res[0][0] || null)
+                .catch(err => {
+                    console.error(err);
+                    return null;
+                });
+
+            if (findOnContacts) res(findOnContacts as { CODIGO: number });
+
+            res(null);
+        });
 
 
         await instance.pool.query(
